e5ef565c9e3ef570385b10df2d46e46ed0b3fbcb
Lea <oleay001@edu.xamk.fi>
2018-12-27 16:39:55 +0200

    27122018.2

--- main.js
+++ main.js
@@ -1,47 +1,25 @@
 const express = require("express");
 const app = express();
-const bodyParser=require("body-parser");
-const ostoslista = require("./models/ostoslista");
 
-const portti = 1000;
+const portti = process.env.PORT || 3103; //Herokulle ei voi määrittää porttia joten se luo oman, tai toimii paikallisesti omassa portissa
+
+const tehtavalista = require("./models/tehtavat"); 
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.use(express.static("./public/"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
 
-app.get("/", (req, res)=>{
-    ostoslista.haeListat((err, data)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("index", {"tiedot":data});
-        }
+    tehtavalista.haeKaikki((data) => {
+
+        res.render("index", { "tehtavat" : data });
+
     });
-});
-
-app.post("/lisaaListaan", (req, res)=>{
-    let tiedot={"kayttajaId":req.body.kayttaja,
-                "listanNimi":req.body.lista,
-                "ostettu":Number(req.body.ostettu),
-                "sisalto":req.body.sisalto
-               };
     
-    ostoslista.lisaaListaan(req.body, (err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.redirect("/");
-        }
-    });
 });
 
-app.listen(portti, ()=>{
-    console.log(`Yhteys on avattu porttiin ${portti}`);
-});
+app.listen(portti, () => {
 
-//kenen lista, mikä lista, jaettu, sisältö, ostettu
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);
+
+});
